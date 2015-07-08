a8d04b53d0dbd8910b4e7f4f1f92a1e79cc1c281
archerImagine <archerimagine@gmail.com>
2015-07-08 22:31:28 +0530

    added themes and the submodules.

--- pelicanconf.py
+++ pelicanconf.py
@@ -35,5 +35,7 @@ DEFAULT_PAGINATION = 10
 
 OUTPUT_RETENTION = [".git"]
 
+THEME = "themes/voidy-bootstrap"
+
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
